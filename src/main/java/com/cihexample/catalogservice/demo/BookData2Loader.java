a35325e414ac95bd281a63f4241bf0a3b3b9a369
choilhyun <cihg155@gmail.com>
2024-09-27 16:46:00 +0900

    [데이터 프로파일 선택적 활성화2] 좀 더 확장 가능하고 구조화된 접근법- @ConditionalOnProperty(name="polar.testdata2.enabled", havingValue = "true")

---
+++ src/main/java/com/cihexample/catalogservice/demo/BookData2Loader.java
@@ -0,0 +1,28 @@
+package com.cihexample.catalogservice.demo;
+
+
+import com.cihexample.catalogservice.domain.Book;
+import com.cihexample.catalogservice.domain.BookRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name="polar.testdata2.enabled", havingValue = "true")
+public class BookData2Loader {
+    private final BookRepository bookRepository;
+
+    public BookData2Loader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class) // 애플리케이션 시작 단계가 완료되면 발생한다.
+    public void loadBookTestData(){
+        var book1 = new Book("1234567893", "Northedn dfsd2", "lydf sdf2", 9.90);
+        var book2 = new Book("1234567894", "woman life2", "il fdfee2", 12.90);
+
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
