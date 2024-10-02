96ac69d99babd9e87cdfbf3553a8d017e33271cd
choilhyun <cihg155@gmail.com>
2024-10-02 14:53:12 +0900

    [플라이웨이 초기 작업-p207] 의존성 추가, 최초 스크립트 버전 등록

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,18 @@
-DROP TABLE IF EXISTS book;
-CREATE TABLE book(
- id                     BIGSERIAL PRIMARY KEY NOT NULL,
- author                 varchar(255) NOT NULL,
- isbn                    varchar(255) UNIQUE NOT NULL,
- price                   float8 NOT NULL,
- title                   varchar(255) NOT NULL ,
- created_date            timestamp NOT NULL,
- last_modified_date      timestamp NOT NULL,
- version                 integer NOT NULL
-)
+-- 플라이웨이 사용으로 주석 처리
+-- 플라이웨이 사용으로 주석 처리
+-- 플라이웨이 사용으로 주석 처리
+-- 플라이웨이 사용으로 주석 처리
+-- 플라이웨이 사용으로 주석 처리
+
+
+-- DROP TABLE IF EXISTS book;
+-- CREATE TABLE book(
+--  id                     BIGSERIAL PRIMARY KEY NOT NULL,
+--  author                 varchar(255) NOT NULL,
+--  isbn                    varchar(255) UNIQUE NOT NULL,
+--  price                   float8 NOT NULL,
+--  title                   varchar(255) NOT NULL ,
+--  created_date            timestamp NOT NULL,
+--  last_modified_date      timestamp NOT NULL,
+--  version                 integer NOT NULL
+-- )
